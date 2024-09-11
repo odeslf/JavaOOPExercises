afab4dd68d47a9c362f4da50ae1345f9f7dcd58c
Felipe Ribeiro <lipe13silva@hotmail.com>
2024-09-11 15:51:35 -0300

    Java OOP Exercise

---
+++ Classes Exercises/src/TheInvoiceItem.java
@@ -0,0 +1,78 @@
+public class TheInvoiceItem {
+
+    private String id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+
+    public TheInvoiceItem(String id, String desc, int qty, double unitPrice) {
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public int getQty() {
+        return this.qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getUnitPrice() {
+        return this.unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getTotal() {
+        return unitPrice * qty;
+    }
+
+    public String total() {
+        return "O total da compra é de " + getTotal();
+    }
+
+    @Override
+    public String toString() {
+        return "TheInvoiceItem{" +
+                "id='" + id + '\'' +
+                ", desc='" + desc + '\'' +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
+class InvoiceItemTester {
+    public static void main(String[] args) {
+        // Test constructor and toString()
+        TheInvoiceItem inv1 = new TheInvoiceItem("A101", "Pen Red", 888, 0.08);
+        System.out.println(inv1);  // toString();
+
+        // Test Setters and Getters
+        inv1.setQty(999);
+        inv1.setUnitPrice(0.99);
+        System.out.println(inv1);  // toString();
+        System.out.println("id is: " + inv1.getId());
+        System.out.println("desc is: " + inv1.getDesc());
+        System.out.println("qty is: " + inv1.getQty());
+        System.out.println("unitPrice is: " + inv1.getUnitPrice());
+
+        // Test getTotal()
+        System.out.println("The total is: " + inv1.getTotal());
+    }
+}
+
+
+
