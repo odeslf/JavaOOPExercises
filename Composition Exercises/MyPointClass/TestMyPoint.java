cbd03b8b46c748a7628e127008ce3fbf65e7d8f2
Felipe Ribeiro <lipe13silva@hotmail.com>
2024-09-18 14:48:26 -0300

    Composition Exercises

--- Composition Exercises/MyPointClass/TestMyPoint.java
+++ Composition Exercises/MyPointClass/TestMyPoint.java
@@ -4,14 +4,24 @@ public class TestMyPoint {
 
     public static void main(String[] args) {
 
-        MyPoint[] points = new MyPoint[10];
+//        MyPoint[] points = new MyPoint[10];
+//
+//        for (int i = 0; i < points.length; i++) {
+//            points[i] = new MyPoint(i + 1, i + 1);
+//        }
+//
+//        for (MyPoint point : points) {
+//            System.out.println(point.toString() + " Distance to origin: " + point.distance());
+//        }
 
-        for (int i = 0; i < points.length; i++) {
-            points[i] = new MyPoint(i + 1, i + 1);
-        }
 
-        for (MyPoint point : points) {
-            System.out.println(point.toString() + "Distance to origin: " + point.distance());
-        }
+        MyPoint p1 = new MyPoint(0,0);
+        MyPoint p2 = new MyPoint(3,4);
+
+        MyLine line = new MyLine(p1, p2);
+
+
+        System.out.println("Begin point: " + line.getBegin());
+        System.out.println("End point: " + line.getEnd());
     }
 }
