b86d8c3b03c7b38d7486be024f752a94b5e9b567
Felipe Ribeiro <lipe13silva@hotmail.com>
2024-09-19 13:34:48 -0300

    Composition Exercises

--- Exercises on Polymorphism, Abstract Classes and Interfaces/PolymorphismAbstractClassAndInterfaces/src/GeometricObj/GeometricObject.java
+++ Exercises on Polymorphism, Abstract Classes and Interfaces/PolymorphismAbstractClassAndInterfaces/src/GeometricObj/GeometricObject.java
@@ -2,6 +2,6 @@ package GeometricObj;
 
 public interface GeometricObject {
 
-    abstract double getArea();
-    abstract double getPerimeter();
+     double getArea();
+     double getPerimeter();
 }
